c0a950721b78352b0c222a6e9ff221f48c73e41b
Jackson DeDobbelaere <jackdedobb@me.com>
2018-03-26 23:54:52 -0500

    starting to repair everything

--- sign_up.php
+++ sign_up.php
@@ -1,7 +1,11 @@
 <?php
 
 
+$mysqli = new mysqli("localhost", "cs411demo_dude", "letmein!", "cs411demo_genes");
 
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 
 $q = $_GET["email"];
 
