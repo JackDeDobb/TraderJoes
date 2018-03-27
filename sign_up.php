eff8a418488c79e6a499e06f0e6704dd7ef6bb0b
asypereddi <asypereddi@gmail.com>
2018-03-27 18:27:16 -0500

    YEs

--- sign_up.php
+++ sign_up.php
@@ -4,11 +4,9 @@ $user = $_GET["username"];
 $pass = $_GET["password"];
 $email = $_GET["email"];
 
-print $user
-print $pass
-print $email
-
-
+print $user;
+print $pass;
+print $email;
 
 
 echo "Failed to connect to MySQL: (" $q;
