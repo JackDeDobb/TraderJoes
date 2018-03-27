79872e79ab5ca7264d5900f91e4a1d25314b7906
Jackson DeDobbelaere <jackdedobb@me.com>
2018-03-27 18:51:36 -0500

    working on sign_up

--- sign_up.php
+++ sign_up.php
@@ -1,4 +1,5 @@
 <?php
+include 'credentials.php';
 
 $user = $_GET["username"];
 $pass = $_GET["password"];
@@ -7,5 +8,25 @@ $email = $_GET["email"];
 print $user;
 print $pass;
 print $email;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT 1 FROM table WHERE key = $user;";
+if ($conn->query($sql) === 0) {
+    echo "Beginning to Create User\n";
+} else {
+    echo "User already exists";
+    return;
+}
+
+
+
+
 
 ?>
