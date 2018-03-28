cf5908eba6b9072676f39e64ddab8d4c3d13deae
Rahul <rshah98626@gmail.com>
2018-03-27 21:22:27 -0500

    Minor bug stock search

--- processInput.php
+++ processInput.php
@@ -6,7 +6,7 @@ $q = $_GET["symbol"];
 //$attr = $_GET["attr"];
 
 echo "Hello Wrold";
-
+/*
 $url = "https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols="
 $url .= $q;
 $url .= "&apikey=S4TYOA5YDZJBLT1K"
@@ -17,5 +17,5 @@ var_dump(json_decode($result, true));
 $price = $array["Stock Quotes"]["0"]["2. price"];
 
 // Output "no suggestion" if no hint was found or output correct values
-print "<h2>Response from server: {$price}</h2>";
+print "<h2>Response from server: {$price}</h2>";*/
 ?>
