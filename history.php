af735cbad7ab4fa0b26fae7813b70743ad89080b
Rahul <rshah98626@gmail.com>
2018-03-28 11:21:44 -0500

    Portfolio PHP

---
+++ history.php
@@ -0,0 +1,23 @@
+<?php
+include 'credentials.php';
+$user = $_SESSION['login_user'];
+
+$tableString = "";
+
+$sql = "SELECT * FROM Stocks WHERE username = '$user'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $tableString .= "<table><tr><th>Symbol</th><th>Quantity</th><th>Total Investment</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $tableString .= "<tr><td>" . $row["ticker_symbol"]. "</td><td>" . $row["quantity_stocks"]. "</td><td>" . $row["total_investment"]. "</td></tr>";
+    }
+    $tableString .= "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+echo $tableString;
+?>
