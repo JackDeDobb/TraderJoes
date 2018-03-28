6d85bb1bd4f5ca0995b8bf522be0aee54efadbbb
Jackson DeDobbelaere <jackdedobb@me.com>
2018-03-27 21:14:14 -0500

    good checkpoint here

--- sign_up.php
+++ sign_up.php
@@ -13,7 +13,7 @@ $email = $_GET["email"];
 
 echo "Beginning to create user.";
 $sql = "INSERT INTO User VALUES ('$user', '$name', '$pass')"; $conn->query($sql);
-$sql = "INSERT INTO Profile VALUES ('$user', NOW(), '$age', '$gender', '$email')"; $conn->query($sql);
+$sql = "INSERT INTO Profile VALUES ('$user', CURDATE(), '$age', '$gender', '$email')"; $conn->query($sql);
 $sql = "INSERT INTO Game VALUES ('$user', CURRENT_TIMESTAMP)"; $conn->query($sql);
 $sql = "INSERT INTO PlayerAssets VALUES ('$user', 5000)"; $conn->query($sql);
 
