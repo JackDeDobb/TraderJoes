d7b234fa5bbc98e346b8f81524dfd2b57dea281f
asypereddi <asypereddi@gmail.com>
2018-03-27 22:57:18 -0500

    Buy

--- buy.php
+++ buy.php
@@ -1,7 +1,7 @@
 <?php
 include 'credentials.php';
 
-$q = $_GET["symbol"];
+// $q = $_GET["symbol"];
 
 // $url = "https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=";
 // $url .= $q;
@@ -10,15 +10,18 @@ $q = $_GET["symbol"];
 
 // $array = json_decode($result, true);
 //$price = $array["Stock Quotes"]["0"]["2. price"];
+print "Started"
 $user = $_SESSION['login_user'];
 $price = 100;
 $quantity = 2;
+print $user
 
-print "<h2>Response from server: {$price}</h2>";
+// print "<h2>Response from server: {$price}</h2>";
 
 $sql = "INSERT INTO Stocks VALUES ('$user', '$q', '$quantity' , '$price')
 		/*ON DUPLICATE KEY UPDATE 
 		Stocks.quantity_stocks = Stocks.quantity_stocks + VALUES(quantity_stocks),
-		Stocks.prev_money_made = Stocks.prev_money_made + VALUES(prev_money_made)**/; $conn->query($sql);
+		Stocks.prev_money_made = Stocks.prev_money_made + VALUES(prev_money_made)**/"; $conn->query($sql);
+print "Ended"
 
 ?>
