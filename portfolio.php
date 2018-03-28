0f9e7a838dabc7ebe311d80c557e60337ac678fe
Rahul <rshah98626@gmail.com>
2018-03-28 12:12:55 -0500

    Avg Val added and Cash left

--- portfolio.php
+++ portfolio.php
@@ -4,20 +4,24 @@ session_start();
 $user = $_SESSION['login_user'];
 //echo $user;
 
-$tableString = "";
+$sql = "SELECT * FROM PlayerAssets WHERE username = '$user'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$tableString = "<h2>Liquid Assets: $" . $row["cash"] . "</h2>";
 
 $sql = "SELECT * FROM Stocks WHERE username = '$user'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    $tableString .= "<table><tr><th>Symbol</th><th>Quantity</th><th>Total Investment</th></tr>";
+    $tableString .= "<table><tr><th>Symbol</th><th>Quantity</th><th>Total Investment</th><th>Average Value</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        $tableString .= "<tr><td>" . $row["ticker_symbol"]. "</td><td>" . $row["quantity_stocks"]. "</td><td>" . $row["total_investment"]. "</td></tr>";
+        $avgVal = $row["total_investment"] / $row["quantity_stocks"];
+        $tableString .= "<tr><td>" . $row["ticker_symbol"]. "</td><td>" . $row["quantity_stocks"]. "</td><td>" . $row["total_investment"]. "</td><td>" . $avgVal . "</td></tr>";
     }
     $tableString .= "</table>";
 } else {
-    echo "0 results";
+    echo "No Stocks";
 }
 
 $conn->close();
