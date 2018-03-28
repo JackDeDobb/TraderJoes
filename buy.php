fbbf5bcf0c50d4b39076303e5f642d3e8973fb66
Jackson DeDobbelaere <jackdedobb@me.com>
2018-03-28 00:45:46 -0500

    fixed buy and sell passing symbol

--- buy.php
+++ buy.php
@@ -1,23 +1,23 @@
 <?php
 include 'credentials.php';
 
- $symbol = $_GET["q"];
+$symbol = $_GET["q"];
 
-// $url = "https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=";
-// $url .= $q;
-// $url .= "&apikey=S4TYOA5YDZJBLT1K";
-// $result = file_get_contents($url);
+$url = "https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=";
+$url .= $symbol;
+$url .= "&apikey=S4TYOA5YDZJBLT1K";
+$result = file_get_contents($url);
 
-// $array = json_decode($result, true);
-//$price = $array["Stock Quotes"]["0"]["2. price"];
+$array = json_decode($result, true);
+$price = $array["Stock Quotes"]["0"]["2. price"];
 session_start();
 print "Started";
 $user = $_SESSION['login_user'];
 //$q = $_SESSION['session_name'];
-$price = 100;
-$quantity = 2;
+$quantity = 1;
 print $user;
 print $symbol;
+print $price;
 
 // print "<h2>Response from server: {$price}</h2>";
 
