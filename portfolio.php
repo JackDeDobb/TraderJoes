d13922bd2e7d81ae14fb83e82c638cf8f4f9d7a2
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-22 17:04:23 -0500

    successfully added user charts and formated money

--- portfolio.php
+++ portfolio.php
@@ -3,6 +3,7 @@ include 'credentials.php';
 session_start();
 $user = $_SESSION['login_user'];
 //echo $user;
+echo $user
 if(strcmp($user, "") == 0) { echo "hi"; }
 
 $sql = "SELECT * FROM PlayerAssets WHERE username = '$user'";
