d1a6cdc9f7b5e449d4b3c444379640e3e37466f9
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-22 15:20:34 -0500

    successfully added user checks

--- portfolio.php
+++ portfolio.php
@@ -119,7 +119,7 @@ echo $tableString;
 
     <body>
       <div style="width: 100%; overflow: hidden;">
-          <div id="piechart" style="width: 600px; float: left; height: 500px;"></div>
+          <div id="piechart" style="width: 900px; float: left; height: 500px;"></div>
           <div id="piechart2" style="margin-left: 900px; height: 500px;"></div>
       </div>
     </body>
