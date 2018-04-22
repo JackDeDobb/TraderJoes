0b0d76ed52a55341c653a803a6bd112ade36e240
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-22 14:34:03 -0500

    testing existing user

--- sign_in.php
+++ sign_in.php
@@ -1,8 +1,16 @@
 <?php
+include 'credentials.php';
 
-$q = $_GET["username"];
+session_start();
 
-print "<h2>YEEE!</h2>";
-print $q;
+$user = $_GET["username"];
+$pass = $_GET["password"];
+
+//Throw an error if that username exists
+$sql = "SELECT * FROM User WHERE username = '$user'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+if($row == false){ echo "Error: User " . $user . " does not exist!"; return; }
+
 
 ?>
