8b101ee0ce2bde2083fdcbfe196b8ac52b93b206
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-22 16:42:53 -0500

    successfully added user checks

--- portfolio.php
+++ portfolio.php
@@ -70,7 +70,7 @@ echo $tableString2;
                 $totalInvestmentNum = 0;
                 while($row = $result->fetch_assoc()) {
                     $avgVal = $row["total_investment"] / $row["quantity_stocks"];
-                    $tableString .= "<tr><td>" . $row["ticker_symbol"]. "</td><td>" . $row["quantity_stocks"]. "</td><td>" . $row["total_investment"]. "</td><td>" . $avgVal . "</td></tr>";
+                    $tableString .= "<tr><td>" . $row["ticker_symbol"]. "</td><td>" . $row["quantity_stocks"]. "</td><td>" . money_format('%i', $row["total_investment"]) . "</td><td>" . $avgVal . "</td></tr>";
                     echo "['" . $row["ticker_symbol"] . "'," . $row["total_investment"] . "],";
                     $totalInvestmentNum += $row["total_investment"] ;
                 }
