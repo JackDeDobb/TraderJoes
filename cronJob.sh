006c46c47885c35e7af9dcc3debf65a84fa8a3cd
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-22 14:38:31 -0500

    testing existing user

--- cronJob.sh
+++ cronJob.sh
@@ -3,6 +3,7 @@
 cd ~/public_html
 git pull
 chmod 644 sign_up.php
+chmod 644 sign_in.php
 chmod 644 processInput.php
 chmod 644 buy.php
 chmod 644 sell.php
--- sign_in.php
+++ sign_in.php
@@ -6,11 +6,14 @@ session_start();
 $user = $_GET["username"];
 $pass = $_GET["password"];
 
-//Throw an error if that username exists
+//Throw an error if the username exists
 $sql = "SELECT * FROM User WHERE username = '$user'";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 if($row == false){ echo "Error: User " . $user . " does not exist!"; return; }
+
+//Throw an error if the password does not match password in database
+echo $row.username
 
 
 ?>
