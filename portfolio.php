1fcef308f95b060897ea71cc2f654367a8df7715
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-22 15:04:08 -0500

    successfully added user checks

--- portfolio.php
+++ portfolio.php
@@ -62,6 +62,36 @@ echo $tableString;
 		<!--HERE INSERT ALL CODE FOR PORTFOLIO-->
 
 
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+          google.charts.load('current', {'packages':['corechart']});
+          google.charts.setOnLoadCallback(drawChart);
+
+          function drawChart() {
+
+            var data = google.visualization.arrayToDataTable([
+              ['Task', 'Hours per Day'],
+              ['Work',     11],
+              ['Eat',      2],
+              ['Commute',  2],
+              ['Watch TV', 2],
+              ['Sleep',    7]
+            ]);
+
+            var options = {
+              title: 'My Daily Activities'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+            chart.draw(data, options);
+          }
+        </script>
+
+
+
+
+
 		 <!--Footer-->
 			<footer id="footer">
 				<div class="inner">
