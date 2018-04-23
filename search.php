033682f04dae1b92e9c0e8c56f39537aaba0e318
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-22 22:05:36 -0500

    successfully added user charts and formated money

--- search.php
+++ search.php
@@ -232,6 +232,63 @@
 
 
 
+				<form method="post" action="#">
+					<div class="row uniform">
+						<div class="6u 12u$(xsmall)">
+							<input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
+						</div>
+						<div class="6u$ 12u$(xsmall)">
+							<input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
+						</div>
+						<!-- Break -->
+						<div class="12u$">
+							<div class="select-wrapper">
+								<select name="demo-category" id="demo-category">
+									<option value="">- Category -</option>
+									<option value="1">Manufacturing</option>
+									<option value="1">Shipping</option>
+									<option value="1">Administration</option>
+									<option value="1">Human Resources</option>
+								</select>
+							</div>
+						</div>
+						<!-- Break -->
+						<div class="4u 12u$(small)">
+							<input type="radio" id="demo-priority-low" name="demo-priority" checked>
+							<label for="demo-priority-low">Low Priority</label>
+						</div>
+						<div class="4u 12u$(small)">
+							<input type="radio" id="demo-priority-normal" name="demo-priority">
+							<label for="demo-priority-normal">Normal Priority</label>
+						</div>
+						<div class="4u$ 12u$(small)">
+							<input type="radio" id="demo-priority-high" name="demo-priority">
+							<label for="demo-priority-high">High Priority</label>
+						</div>
+						<!-- Break -->
+						<div class="6u 12u$(small)">
+							<input type="checkbox" id="demo-copy" name="demo-copy">
+							<label for="demo-copy">Email me a copy of this message</label>
+						</div>
+						<div class="6u$ 12u$(small)">
+							<input type="checkbox" id="demo-human" name="demo-human" checked>
+							<label for="demo-human">I am a human and not a robot</label>
+						</div>
+						<!-- Break -->
+						<div class="12u$">
+							<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+						</div>
+						<!-- Break -->
+						<div class="12u$">
+							<ul class="actions">
+								<li><input type="submit" value="Send Message" /></li>
+								<li><input type="reset" value="Reset" class="alt" /></li>
+							</ul>
+						</div>
+					</div>
+				</form>
+
+
     </body>
 
 
