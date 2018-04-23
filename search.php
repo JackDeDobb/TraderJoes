c293d82c9486755b347dfc1e6e08f737cd6c9b47
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-23 15:44:01 -0500

    testing out articles on the search page

--- search.php
+++ search.php
@@ -250,12 +250,13 @@
 				function loadArticles(symbol) {
 					var getRequest = 'http://finance.yahoo.com/rss/headline?s='+ symbol;
 
+document.getElementById("1").innerHTML = symbol;
 					var xmlHttp = new XMLHttpRequest();
     			xmlHttp.onreadystatechange = function() {
         	if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             	callback(xmlHttp.responseText);
     			}
-    			xmlHttp.open("GET", getRequest, true); // true for asynchronous 
+    			xmlHttp.open("GET", getRequest, true); // true for asynchronous
     			xmlHttp.send(null);
 
 					document.getElementById("1").innerHTML = xmlHttp.responseText;
