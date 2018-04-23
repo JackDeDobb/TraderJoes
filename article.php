658a78d46f31191ddc180c672fcbc58747de55f2
asypereddi <asypereddi@gmail.com>
2018-04-22 22:29:39 -0500

    article.php

--- article.php
+++ article.php
@@ -1,27 +1,28 @@
-<?php
-// $myXMLData = file_get_contents("http://finance.yahoo.com/rss/headline?s=msft");
-// $xml=simplexml_load_string($myXMLData);
-
-// if($xml == false) {
-// 	echo "Failed loading XML: ";
-// 	foreach(libxml_get_errors() as $error) {
-// 		echo "<br>", $error->message;
-// 	}
-// }
-// else {
-// 	echo $xml->channel->item[1]->title . "<br>";
-// 	echo $xml->channel->item[1]->description . "<br>";
-// 	echo $xml->channel->item[1]->pubDate . "<br>";
-// 	echo $xml->channel->item[1]->link . "<br>";
-// }
-?>
 
 <!DOCTYPE HTML>
 <html>
 <body>
 <h3>Box</h3>
 	<div class="box">
-		<p>Felis sagittis eget tempus primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Magna sed etiam ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus lorem ipsum dolor sit amet nullam.</p>
+		<p>
+		<?php
+			$myXMLData = file_get_contents("http://finance.yahoo.com/rss/headline?s=msft");
+			$xml=simplexml_load_string($myXMLData);
+
+			if($xml == false) {
+				echo "Failed loading XML: ";
+				foreach(libxml_get_errors() as $error) {
+					echo "<br>", $error->message;
+				}
+			}
+			else {
+				echo $xml->channel->item[1]->title . "<br>";
+				echo $xml->channel->item[1]->description . "<br>";
+				echo $xml->channel->item[1]->pubDate . "<br>";
+				echo $xml->channel->item[1]->link . "<br>";
+			}
+		?>	
+		</p>
 	</div>
 </body>
 </html>
