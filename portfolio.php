91f4c7cbf92f867e68820fa1fddb3e1b5c17804c
asypereddi <asypereddi@gmail.com>
2018-04-23 09:51:55 -0500

    portfolio updates

--- portfolio.php
+++ portfolio.php
@@ -149,6 +149,7 @@ echo $tableString2;
 	<div class="box">
 		<p>
 		<?php
+			echo $stocks;
 			$myXMLData = file_get_contents("http://finance.yahoo.com/rss/headline?s=fb");
 			$xml=simplexml_load_string($myXMLData);
 
