c15a5983939188652b7fea8c3786815ac6a7315f
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-23 19:24:26 -0500

    hi

--- portfolio.php
+++ portfolio.php
@@ -7,7 +7,7 @@ echo "User: " . $user;
 $sql = "SELECT * FROM PlayerAssets WHERE username = '$user'";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
-$tableString2 = "<h2>Liquid Assets: $" . money_format('%i', $row["cash"]) . "</h2>";
+$tableString2 = "<h2>Liquid Assets: $" . money_format('%i', $row["cash"]) . "                     " . money_format('%i', $row["cash"]) . "</h2>";
 $liquidForLater = $row["cash"];
 
 $sql = "SELECT * FROM Stocks WHERE username = '$user'";
