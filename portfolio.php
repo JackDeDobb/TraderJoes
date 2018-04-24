8fcc070a255da72beb9bd096d3e923ac58c1c34b
asypereddi <asypereddi@gmail.com>
2018-04-23 20:50:51 -0500

    ye

--- portfolio.php
+++ portfolio.php
@@ -194,7 +194,7 @@ echo $tableString2;
 				curl_setopt($ch, CURLOPT_POSTFIELDS, "text=" . $xml->channel->item[$i]->description);
 				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 				$server_output = curl_exec ($ch);
-				echo $server_output;
+				echo $server_output . "<br>";
 				echo $xml->channel->item[$i]->description . "<br>";
 				echo "</div>";
 				echo "</a>";
