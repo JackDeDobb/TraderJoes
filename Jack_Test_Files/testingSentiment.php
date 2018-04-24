b88406ee6c3d0ec4e01404f7ecf499543f4bedc4
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-23 20:43:00 -0500

    hi

--- Jack_Test_Files/testingSentiment.php
+++ Jack_Test_Files/testingSentiment.php
@@ -1,16 +1,32 @@
-<?php
 
-$get_request = "http://text-processing.com/api/sentiment/";
-$myXMLData = file_get_contents($get_request);
-$xml=simplexml_load_string($myXMLData);
-if($xml == false) {
-  echo "Failed loading XML: ";
-  foreach(libxml_get_errors() as $error) {
-    echo "<br>", $error->message;
-  }
-}
-else {
-  echo $xml;
-}
+
+
+<?php
+//
+// A very simple PHP example that sends a HTTP POST to a remote site
+//
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"http://text-processing.com/api/sentiment/");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "text=\"Facebook\"");
+
+// in real life you should use something like:
+// curl_setopt($ch, CURLOPT_POSTFIELDS,
+//          http_build_query(array('postvar1' => 'value1')));
+
+// receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+// further processing ....
+echo $server_output
+
+
 
 ?>
