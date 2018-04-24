717c5a1c2069a34bca66f2db1f255307bffdc43b
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-23 21:32:32 -0500

    hi

--- portfolio.php
+++ portfolio.php
@@ -233,45 +233,7 @@ echo $tableString2;
 
     <h3>Related Articles</h3>
 
-	<?php
-		$get_request = "http://finance.yahoo.com/rss/headline?s=" . $stocks;
-		$myXMLData = file_get_contents($get_request);
-		$xml=simplexml_load_string($myXMLData);
-		if($xml == false) {
-			echo "Failed loading XML: ";
-			foreach(libxml_get_errors() as $error) {
-				echo "<br>", $error->message;
-			}
-		}
-		else {
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL,"http://text-processing.com/api/sentiment/");
-			curl_setopt($ch, CURLOPT_POST, 1);
-			$i = 1;
-			while($xml->channel->item[$i] != false) {
-				echo "<a href=\"" . $xml->channel->item[$i]->link . "\">";
-				echo "<div class=\"box\">";
-				echo "<header>";
-				echo "<h5>" . $xml->channel->item[$i]->title . "</h5>";
-				echo "<p>" . $xml->channel->item[$i]->pubDate . "</p>";
-				echo "</header>";
-				curl_setopt($ch, CURLOPT_POSTFIELDS, "text=" . $xml->channel->item[$i]->description);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				$server_output = curl_exec ($ch);
-				$json = json_decode($server_output, true);
-				if ($json['probability']['pos'] >= $json['probability']['neg']) {
-					echo "This article has a positive sentiment!" . "<br><br>";
-				} else {
-					echo "This article has a negative sentiment!" . "<br><br>";
-				}
-				echo $xml->channel->item[$i]->description . "<br>";
-				echo "</div>";
-				echo "</a>";
-				$i++;
-			}
-			curl_close ($ch);
-		}
-	?>
+
 		 <!--Footer-->
 			<footer id="footer">
 				<div class="inner">
