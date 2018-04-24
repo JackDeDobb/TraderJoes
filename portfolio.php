c685dc72827231a3dedca34c396304ee9079bc80
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-23 21:23:02 -0500

    hi

--- portfolio.php
+++ portfolio.php
@@ -122,6 +122,7 @@ echo $tableString2;
 										curl_setopt($ch, CURLOPT_URL,"http://text-processing.com/api/sentiment/");
 										curl_setopt($ch, CURLOPT_POST, 1);
 										$i = 1;
+										$totalPos = 0
 										while($xml->channel->item[$i] != false) {
 											curl_setopt($ch, CURLOPT_POSTFIELDS, "text=" . $xml->channel->item[$i]->description);
 											curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
