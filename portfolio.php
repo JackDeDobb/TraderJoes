aeba4f8cee397295d36a26975a3408f35b6f3bd1
Jackson DeDobbelaere <jackdedobb@me.com>
2018-04-23 21:19:05 -0500

    hi

--- portfolio.php
+++ portfolio.php
@@ -122,16 +122,15 @@ echo $tableString2;
 										curl_setopt($ch, CURLOPT_URL,"http://text-processing.com/api/sentiment/");
 										curl_setopt($ch, CURLOPT_POST, 1);
 										$i = 1;
+										$totalNeg = 0
+										$totalPos = 0
 										while($xml->channel->item[$i] != false) {
 											curl_setopt($ch, CURLOPT_POSTFIELDS, "text=" . $xml->channel->item[$i]->description);
 											curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 											$server_output = curl_exec ($ch);
 											$json = json_decode($server_output, true);
-											if ($json['probability']['pos'] >= $json['probability']['neg']) {
-												echo "This article has a positive sentiment!" . "<br><br>";
-											} else {
-												echo "This article has a negative sentiment!" . "<br><br>";
-											}
+											$totalNeg += $json['probability']['neg'];
+											$totaPos += $json['probability']['pos'];
 											$i++;
 										}
 										curl_close ($ch);
